7e9c0c2f9217bba8f93724f31f398b2540276b45
Kulikov Pavel <kulikov-pavlik@bk.ru>
2024-07-19 15:41:11 +0300

    Added liquibase started create table

---
+++ src/main/java/ru/crystal2033/checkingspringdata/CheckingSpringDataApplication.java
@@ -0,0 +1,13 @@
+package ru.crystal2033.checkingspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckingSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/crystal2033/checkingspringdata/CheckingSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.crystal2033.checkingspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
